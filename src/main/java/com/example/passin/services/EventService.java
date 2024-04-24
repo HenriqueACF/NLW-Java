2260d48145a6a93f38d3de181bde5ded97bd5620
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-04-24 15:46:44 -0300

    feat: criação de eventos

--- src/main/java/com/example/passin/services/EventService.java
+++ src/main/java/com/example/passin/services/EventService.java
@@ -2,12 +2,15 @@ package com.example.passin.services;
 
 import com.example.passin.domain.attendee.Attendee;
 import com.example.passin.domain.event.Event;
+import com.example.passin.dto.event.EventIdDTO;
+import com.example.passin.dto.event.EventRequestDTO;
 import com.example.passin.dto.event.EventResponseDTO;
 import com.example.passin.repositories.AttendeeRepository;
 import com.example.passin.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.Normalizer;
 import java.util.List;
 
 @Service
@@ -21,5 +24,25 @@ public class EventService {
         Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found with ID: " + eventId));
         List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
         return new EventResponseDTO(event, attendeeList.size());
+    }
+
+    public EventIdDTO createEvent(EventRequestDTO eventDTO){
+        Event newEvent = new Event();
+        newEvent.setTitle(eventDTO.title());
+        newEvent.setDetails(eventDTO.details());
+        newEvent.setMaximumAttendees(eventDTO.maximumAttendees());
+        newEvent.setSlug(this.createSlug(eventDTO.title()));
+
+        this.eventRepository.save(newEvent);
+
+        return new EventIdDTO(newEvent.getId());
+    }
+
+    private String createSlug(String text){
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized
+                .replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-");
     }
 }
