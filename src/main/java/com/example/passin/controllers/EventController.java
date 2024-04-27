f64d12b3c09eed67596f59da360903aff4049e7b
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-04-27 17:37:21 -0300

    feat: created attendee endpoint

--- src/main/java/com/example/passin/controllers/EventController.java
+++ src/main/java/com/example/passin/controllers/EventController.java
@@ -1,8 +1,10 @@
 package com.example.passin.controllers;
 
+import com.example.passin.dto.attendee.AttendeesListResponseDTO;
 import com.example.passin.dto.event.EventIdDTO;
 import com.example.passin.dto.event.EventRequestDTO;
 import com.example.passin.dto.event.EventResponseDTO;
+import com.example.passin.services.AttendeeService;
 import com.example.passin.services.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,21 +17,28 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequiredArgsConstructor
 public class EventController {
 
-    private final EventService service;
+    private final EventService eventeService;
+    private final AttendeeService attendeeService;
 
     @GetMapping("/{id}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
-        EventResponseDTO event = this.service.getEventDetail(id);
+        EventResponseDTO event = this.eventeService.getEventDetail(id);
         return ResponseEntity.ok(event);
     }
 
     @PostMapping
     public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body,
                                                   UriComponentsBuilder uriComponentsBuilder){
-        EventIdDTO eventIdDTO = this.service.createEvent(body);
+        EventIdDTO eventIdDTO = this.eventeService.createEvent(body);
 
         var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
 
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{id}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventeeAttendees(@PathVariable String id){
+         AttendeesListResponseDTO attendeesListResponseDTO = this.attendeeService.getEventsAttendee(id);
+        return ResponseEntity.ok(attendeesListResponseDTO);
     }
 }
