4d67e9842009da0d536f0fbaadf6b7f3ed338455
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-04-27 17:05:19 -0300

    feat: created attendee service

--- src/main/java/com/example/passin/repositories/CheckInRepositories.java
+++
@@ -1,8 +0,0 @@
-package com.example.passin.repositories;
-
-
-import com.example.passin.domain.checkin.CheckIn;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckInRepositories extends JpaRepository<CheckIn, Integer> {
-}
--- src/main/java/com/example/passin/services/EventService.java
+++ src/main/java/com/example/passin/services/EventService.java
@@ -6,7 +6,6 @@ import com.example.passin.domain.event.exceptions.EventNotFoundException;
 import com.example.passin.dto.event.EventIdDTO;
 import com.example.passin.dto.event.EventRequestDTO;
 import com.example.passin.dto.event.EventResponseDTO;
-import com.example.passin.repositories.AttendeeRepository;
 import com.example.passin.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,12 +18,12 @@ import java.util.List;
 public class EventService {
 
     private final EventRepository eventRepository;
-    private final AttendeeRepository attendeeRepository;
+    private final AttendeeService attendeeService;
 
     public EventResponseDTO getEventDetail(String eventId){
         Event event = this.eventRepository.findById(eventId)
                 .orElseThrow(() -> new EventNotFoundException("Event not found with ID: " + eventId));
-        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+        List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
         return new EventResponseDTO(event, attendeeList.size());
     }
 
