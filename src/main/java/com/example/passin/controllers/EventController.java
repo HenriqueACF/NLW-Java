90d6319310f183c38661b89d60906767b7794356
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-04-27 22:56:20 -0300

    feat:endpoint de registro do participante

--- src/main/java/com/example/passin/controllers/EventController.java
+++ src/main/java/com/example/passin/controllers/EventController.java
@@ -1,5 +1,7 @@
 package com.example.passin.controllers;
 
+import com.example.passin.dto.attendee.AttendeeIdDTO;
+import com.example.passin.dto.attendee.AttendeeRequestDTO;
 import com.example.passin.dto.attendee.AttendeesListResponseDTO;
 import com.example.passin.dto.event.EventIdDTO;
 import com.example.passin.dto.event.EventRequestDTO;
@@ -34,6 +36,16 @@ public class EventController {
         var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
 
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body,
+                                                  UriComponentsBuilder uriComponentsBuilder){
+        AttendeeIdDTO attendeeIdDTO = this.eventeService.registerAttendeeOnEvent(eventId, body);
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/badge").buildAndExpand(attendeeIdDTO.attendeeId()).toUri();
+
+        return ResponseEntity.created(uri).body(attendeeIdDTO);
     }
 
     @GetMapping("/attendees/{id}")
